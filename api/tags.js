ea9070b13cac9d702e0d098bb74be299c95206fc
Camden Jenkins <camdenjenkins@yahoo.com>
2022-10-28 14:16:08 -0700

    Wrappin up

--- api/tags.js
+++ api/tags.js
@@ -22,6 +22,10 @@ tagsRouter.get("/:tagName/posts", async (req, res, next) => {
   try {
     // use our method to get posts by tag name from the db
     const postsByTag = await getPostsByTagName(tagName);
+
+    const posts = postsByTag.filter((post) => {
+      return post.active || (req.user && post.author.id === req.user.id);
+    });
     // send out an object to the client { posts: // the posts }
     res.send({ posts: postsByTag });
   } catch ({ name, message }) {

b2da1f4d5e9ce48c8a01ec0e358222026e674272
Camden Jenkins <camdenjenkins@yahoo.com>
2022-10-28 13:55:42 -0700

    sanity check

--- api/tags.js
+++ api/tags.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { getAllTags } = require("../db");
 const tagsRouter = express.Router();
+const { getPostsByTagName } = require("../db");
 
 tagsRouter.use((req, res, next) => {
   console.log("A request is being made to /tags");
@@ -13,6 +14,20 @@ tagsRouter.get("/", async (req, res) => {
   res.send({
     tags,
   });
+});
+
+tagsRouter.get("/:tagName/posts", async (req, res, next) => {
+  // read the tagname from the params
+  const { tagName } = req.params;
+  try {
+    // use our method to get posts by tag name from the db
+    const postsByTag = await getPostsByTagName(tagName);
+    // send out an object to the client { posts: // the posts }
+    res.send({ posts: postsByTag });
+  } catch ({ name, message }) {
+    // forward the name and message to the error handler
+    next({ name, message });
+  }
 });
 
 module.exports = tagsRouter;

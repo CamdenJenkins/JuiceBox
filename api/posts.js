a65a8e811ad0cd06a644ef3189a1f340678d8786
Camden Jenkins <camdenjenkins@yahoo.com>
2022-10-28 13:33:53 -0700

    POST api donso

--- api/posts.js
+++ api/posts.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const postsRouter = express.Router();
 const { getAllPosts } = require("../db");
+const { requireUser } = require("./utils");
+const { createPost } = require("../db");
 
 postsRouter.use((req, res, next) => {
   console.log("A request is being made to /posts");
@@ -13,6 +15,39 @@ postsRouter.get("/", async (req, res) => {
   res.send({
     posts,
   });
+});
+
+postsRouter.post("/", requireUser, async (req, res, next) => {
+  const { title, content, tags = "" } = req.body;
+  const { id } = req.user;
+
+  const tagArr = tags.trim().split(/\s+/);
+  const postData = {
+    authorId: id,
+    title,
+    content,
+    tagArr,
+  };
+
+  // only send the tags if there are some to send
+  if (tagArr.length) {
+    postData.tags = tagArr;
+  }
+
+  try {
+    // add authorId, title, content to postData object
+    const post = await createPost(postData);
+    // this will create the post and the tags for us
+    // if the post comes back, res.send({ post });
+    if (post) {
+      res.send({ post });
+    } else {
+      next({ name: "error", message: "Error creating posts" });
+    }
+    // otherwise, next an appropriate error object
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
 });
 
 module.exports = postsRouter;
